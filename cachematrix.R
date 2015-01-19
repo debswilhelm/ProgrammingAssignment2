8d3112c7fd8653fca52b78dba677a825105375c6
Deb Wilhelm <debswilhelm@gmail.com>
2015-01-18 21:33:50 -0500

    Working code to upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,54 +1,15 @@
 ## R Programming Course - Assignment #2
-## D. Wilhelm  1/18/2015
-##
-## This function will ...
-##
+## debswilhelm  1/18/2015
 ##
 
-
-## The first function, makeVector creates a special "vector", 
+## The first function, makeCacheMatrix creates a special "matrix", 
 ## which is really a list containing a function to
 
-## set the value of the vector
-## get the value of the vector
-## set the value of the mean
-## get the value of the mean
+## set the value of the matrix
+## get the value of the matrix
+## set the value of the inverse (solve)
+## get the value of the inverse (solve)
 
-makeVector <- function(x = numeric()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
-  list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
-}
-
-## The following function calculates the mean of the special "vector" 
-## created with the above function. However, it first checks to see if 
-## the mean has already been calculated. If so, it gets the mean from 
-## the cache and skips the computation. Otherwise, it calculates the 
-## mean of the data and sets the value of the mean in the cache via the 
-## setmean function.
-
-cachemean <- function(x, ...) {
-  m <- x$getmean()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
-  m
-}
-
-
-## Write a short comment describing this function
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -64,7 +25,12 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the matrix inverse of the special "matrix" 
+## created with the above function. However, it first checks to see if 
+## the inverse has already been calculated. If so, it gets the inverse from 
+## the cache and skips the computation. Otherwise, it calculates the 
+## inverse of the data and sets the value of the inverse in the cache via the 
+## solve function.
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
